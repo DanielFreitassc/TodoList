1edc42cb6dc3b2e9b55ee4df1c6ba48a6a5f0395
danielfreitassc <danielfreitassc@gmail.com>
2024-01-04 22:30:22 -0300

    att

--- src/main/java/danielfreitassc/todolist/controllers/UserController.java
+++ src/main/java/danielfreitassc/todolist/controllers/UserController.java
@@ -2,6 +2,7 @@ package danielfreitassc.todolist.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,6 @@ public class UserController {
         System.out.println(user.getName());
     }
     @GetMapping("/buscar")
+    @PutMapping
     
 }
